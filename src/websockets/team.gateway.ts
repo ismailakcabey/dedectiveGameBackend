c7c097550ca5fcdec707236202d05587c821a6ac
ismailakca <ismailakca399@gmail.com>
2023-07-25 13:41:39 +0300

    update websocket

--- src/websockets/team.gateway.ts
+++ src/websockets/team.gateway.ts
@@ -2,8 +2,24 @@ import { OnModuleInit } from '@nestjs/common';
 import { WebSocketGateway, WebSocketServer, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({cors:true})
 export class TeamGateway implements OnModuleInit {
+
+    constructor() {
+        this.server = new Server(); // Yeni bir socket.io sunucusu oluşturun
+      }
+
+      afterInit(server: Server) {
+        console.log('WebSocket server initialized');
+      }
+  
+      handleConnection(client: Socket) {
+        console.log(`Client connected: ${client.id}`);
+      }
+  
+      handleDisconnect(client: Socket) {
+        console.log(`Client disconnected: ${client.id}`);
+      }
 
     onModuleInit() {
         this.server.on('connection', (socket) => {
@@ -19,16 +35,14 @@ export class TeamGateway implements OnModuleInit {
     onNewMessage(
         @MessageBody() body: {
             sender: string,
-            contet: string,
+            content: string,
             teamId: string
         }
     ) {
         console.log(body)
         this.server.emit('listenMessage/' + body.teamId, {
-
             ...body,
             createdAt: new Date(), // Yeni createdAt özelliği
-
         })
     }
 
